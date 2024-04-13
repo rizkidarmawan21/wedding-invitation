1444ce79ef8114b71a576e5d5ab39a62b68f74db
rizkidarmawan21 <rizkidarmawan.0402102@gmail.com>
2024-04-13 09:12:24 +0700

    Add countdown timer functionality

--- alpine/app.js
+++ alpine/app.js
@@ -4,9 +4,12 @@ function app() {
         isLoadingCover: false,
         music: false,
         audio: '',
+        timeLeft: { days: 0, hours: 0, minutes: 0, seconds: 0 },
 
         init() {
             this.audio = document.getElementById('myAudio');
+
+            this.countdownTimer();
         },
 
         handleOpenInvitation() {
@@ -33,7 +36,33 @@ function app() {
         handleMusic() {
             this.music = !this.music;
             this.music ? this.audio.play() : this.audio.pause();
+        },
+
+        // Reference: https://www.w3schools.com/howto/howto_js_countdown.asp
+        countdownTimer() {
+            const targetDate = new Date("2025-04-13T10:00:00").getTime();
+
+            const updateCountdown = () => {
+                const now = new Date().getTime();
+                const difference = targetDate - now;
+
+                if (difference > 0) {
+                    this.timeLeft = {
+                        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+                        hours: Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+                        minutes: Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60)),
+                        seconds: Math.floor((difference % (1000 * 60)) / 1000)
+
+                    };
+                } else {
+                    this.timeLeft = null;
+                }
+            }
+
+            updateCountdown();
+            setInterval(updateCountdown, 1000);
         }
+
 
 
     }
